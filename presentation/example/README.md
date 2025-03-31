43afb80db89320d5db65ccdde134632ac6cd69c4
Andreas Meid <meid.andreas@gmail.com>
2025-03-31 11:56:02 +0200

    Add presentation

---
+++ presentation/example/README.md
@@ -0,0 +1,6 @@
+
+# Example title
+
+This is an attempt to implement a quarto presentation, taking the example of
+
+[![Title slide screenshot](index-img.png)](https://jthomasmock.github.io/arrow-dplyr/)
---
+++ presentation/example/generate-meta.R
@@ -0,0 +1,8 @@
+metathis::meta() |> 
+  metathis::meta_social(
+    title = "Example title",
+    description = "Example description",
+    url = "https://andreasmeid.github.io/presentation/example",
+    image = "index-img.png",
+    image_alt = "The title slide of the presentation"
+  )
